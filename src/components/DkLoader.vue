559663f5789e6695ee780d5bdcae1bdb1a42585d
DkVZ <bouc@clanz.be>
2019-03-25 13:37:16 +0100

    Created a loader component with a simple CSS animation

---
+++ src/components/DkLoader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="loading" class="jumbotron center-it">
+    <h1 class="display-4">Chargement...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DkLoader',
+  props: {
+    loading: Boolean
+  }
+}
+</script>
+
+<style scoped>
+.center-it {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+@keyframes scaler {
+  0% {transform: scale(1)}   
+  50% {transform: scale(1.25)}   
+  100% {transform: scale(1)}
+}
+
+.center-it h1 {
+  animation: scaler 0.6s infinite; 
+}
+</style>
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="container">
-    <h1>Planning</h1>
+    <h1>{{ planningName }}</h1>
+    <DkLoader :loading="loading"></DkLoader>
   </div>
 </template>
 
 <script>
+import api from '@/api';
+import DkLoader from '@/components/DkLoader.vue';
+
 export default {
   name: 'Planning',
   props: {
     isAuthenticated: Boolean
+  },
+  data: function() {
+    return {
+      planningName: 'Chargement...',
+      loading: true,
+      events: []
+    };
   },
   beforeRouteEnter: function(to, from, next) {
     // The component instance is not accessible through "this"
@@ -16,7 +27,8 @@ export default {
     //console.log(to, from);
     next(self => {
       // The component instance is in "self".
-      
+      // Load the planning data:
+
     });
   }
 }
